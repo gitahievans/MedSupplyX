67ed9df71230ab14b9ef4cd0314a95d7cf93ffa3
gitahievans <gitahievance@gmail.com>
2023-04-29 10:56:21 +0300

    Style first product on homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -13,35 +13,29 @@ const Homepage = () => {
       }, []);
 
     return (
-      <main className="flex flex-col place-items-center">
+      <main className="bg-[#FBF5FF] flex flex-col items-center mx-auto md:max-w-[1200px] gap-8">
         {" "}
         {/* Entire homepage content */}
-        <section className="  flex flex-col place-items-center border-b-2 py-4 px-4 rounded-b-2xl lg:flex-row lg:py-36">
+        <section className="bg-white mt-8 flex flex-col md:flex-row items-center gap-12 rounded-xl">
           {/* New product */}
-          <div className=" pb-2 pt-2 flex justify-center">
-            <img
-              src={bpbg}
-              alt="bp monitor"
-              className="h-1/2 w-1/2 lg:w-full lg:h-full"
-            />
+          <div className="p-4 flex items-center justify-center">
+            <img src={bpbg} alt="bp monitor" className="h-56 md:h-96 md:w-96" />
           </div>
-          <div className="w-full text-[#1E194F] flex flex-col place-items-center text-center gap-4">
-            <span className="text-slate-300 font-extrabold text-3xl italic">
-              NEW PRODUCT
-            </span>
-            <h1 className=" font-extrabold text-4xl">BLOOD PRESSURE MONITOR</h1>
-            <p className=" font-bold text-2xl">
+          <div className=" w-3/5 text-[#1E194F] flex flex-col items-center text-center gap-8 px-8">
+            <p className="font-extrabold text-2xl">NEW PRODUCT</p>
+            <h1 className="font-medium text-xl">BLOOD PRESSURE MONITOR</h1>
+            <p className=" font-normal text-base">
               Take control of your health and monitor your blood pressure at
               home with our reliable and easy-to-use blood pressure monitor
             </p>
             <Link to="/equipment#bp">
-              <button className="h-12 w-40 bg-white border-2 hover:text-white hover:bg-[#1E194F] transition ease-in-out duration-500  rounded-2xl text-xl font-extrabold">
+              <button className="h-12 w-40 bg-white border-2 hover:text-white hover:bg-[#1E194F] transition ease-in-out duration-500  rounded-2xl text-xl font-semibold">
                 View Product
               </button>
             </Link>
           </div>
         </section>
-        <section className="flex flex-col gap-12 place-items-center my-24 w-10/12 lg:grid lg:grid-cols-3 lg:w-11/12">
+        <section className="bg-white flex  gap-12 items-center ">
           {/* Different sections */}
           <div className=" bg-gray-200 rounded-2xl w-full flex flex-col place-items-center my-8 relative lg:col-span-1 lg:w-full">
             {/* Hospital equipment section */}
