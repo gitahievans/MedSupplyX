a8da28cb04c326b3d5d7779c6225fdaee4f760ca
gitahievans <gitahievance@gmail.com>
2023-04-29 13:20:16 +0300

    Updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { useContext } from "react";
 import { GlovesValueContext } from "./context/GlovesValueContext";
 import { TotalValueContext } from "./context/TotalValueContext";
 import hamburgerIcon from "../assets/icons/hamburger-menu.svg"
+import cart from "../assets/icons/cart.svg";
 const Navigation = () => {
   const {
     glovesValue,
@@ -19,15 +20,11 @@ const Navigation = () => {
   const [nav, setNav] = useState(false);
 
   const toggleNav = () => {
-    if (!nav) {
-      setNav(true);
-    } else {
-      setNav(false);
-    }
+    setNav(!nav)
   };
 
   return (
-    <header className=" py-8 w-full text-[#1E194F] flex flex-col justify-around border-b-2 gap-8">
+    <header className=" py-8 w-full text-[#1E194F] flex flex-col justify-around border-b-2 gap-8 ">
       <div className="flex justify-around place-items-center">
         <img
           src={hamburgerIcon}
@@ -40,20 +37,7 @@ const Navigation = () => {
         </Link>
         <Link to="/checkout">
           <div className="flex">
-            <svg
-              className="w-10 h-10"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
-              />
-            </svg>
+            <img src={cart} alt="" className="w-10 h-10" />
             <h1 className="text-center text-2xl font-extrabold">
               {totalValue}
             </h1>
@@ -61,7 +45,7 @@ const Navigation = () => {
         </Link>
       </div>
       {nav && (
-        <div className=" flex flex-col justify-around text-center text-xl gap-3 font-bold mt-4 lg:flex-row">
+        <div className=" flex flex-col justify-around text-center text-xl gap-3 font-bold mt-4 lg:flex-row ">
           <Link to="/" className="flex flex-col justify-center">
             Home
           </Link>
